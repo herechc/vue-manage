957c42e77964d87a6b970e17b9af3c9e94a9aefe
chc <hanlike721@163.com>
2017-10-07 20:09:55 +0800

    add datamanage

--- src/views/add/addGoods.vue
+++ src/views/add/addGoods.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="container">
-    添加商品
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6">
+        <el-form label-width="120px">
+          <el-form-item label="食品种类：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="种类描述：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="食品名称：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="食品详情：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="食品价格：">
+            <el-input-number :min="10"></el-input-number>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">立即创建</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
--- src/views/add/index.vue
+++ src/views/add/index.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="container">
-    添加商铺
+    <el-row :gutter="20">
+      <el-col :span="12" :offset="6">
+        <el-form label-width="120px">
+          <el-form-item label="店铺名称：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="详细地址：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="联系电话：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="店铺简介：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="店铺标语：">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary">立即创建</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
   export default {
-    name: 'addgoods'
+    name: 'addshop'
   }
 </script>
 <style lang="scss" scoped></style>
